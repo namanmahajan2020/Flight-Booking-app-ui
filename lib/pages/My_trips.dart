a3b7deb43d711bfbb65727f8d145ed240b4d9340
Naman Mahajan <mahajannaman2020@gmail.com>
2025-04-04 23:52:16 +0530

    my trips page

--- lib/pages/My_trips.dart
+++ lib/pages/My_trips.dart
@@ -1,9 +1,118 @@
+import 'package:flight_booking/components/places_template.dart';
 import 'package:flutter/material.dart';
+
 class MyTrips extends StatelessWidget {
   const MyTrips({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+          ),
+          Container(height: 172, color: Color.fromARGB(255, 234, 239, 240)),
+          Container(
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24.0, right: 24, top: 24),
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey[300] ?? Colors.grey,
+                              width: 0.5,
+                            ),
+
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(Icons.add),
+                        ),
+                        Text(
+                          "My Trips",
+                          style: TextStyle(
+                            fontSize: 19,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.grey, width: 0.5),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.asset(
+                              "lib/images/profile.jpg",
+                              height: 45,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 31),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 55,
+                          width: 173,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey[300] ?? Colors.grey,
+                              width: 1,
+                            ),
+                            color: Color(0xFF134748),
+
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Next Trip",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 55,
+                          width: 173,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey[300] ?? Colors.grey,
+                              width: 1,
+                            ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "History",
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
